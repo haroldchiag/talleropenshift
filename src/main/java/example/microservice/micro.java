d5c76b9b054c3d3c403f98ef975d0445c2fa8985
sala_c <sala_c@10.180.12.103>
2016-11-05 11:30:44 -0500

    ok2

--- src/main/java/example/microservice/micro.java
+++ src/main/java/example/microservice/micro.java
@@ -1,5 +1,8 @@
 package example.microservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
@@ -18,4 +21,35 @@ public class micro {
 	    System.out.println("sumar: " + (a+b));
 	    return (a+b);
 	}
+	
+	@WebMethod()
+    public List<placas> getPlacas() {
+	 List<placas> b = new ArrayList<placas>();
+	 placas bo = new placas();
+	 bo.setNumeroPlaca("abc123");
+	 bo.setEsRobada(false);
+	 b.add(bo);
+    
+	 bo = new placas();
+	 bo.setNumeroPlaca("efg456");
+	 bo.setEsRobada(true);
+	 b.add(bo);
+	 
+	 bo = new placas();
+	 bo.setNumeroPlaca("hij789");
+	 bo.setEsRobada(false);
+	 b.add(bo);
+	 
+	 return b;
+    }
+ 
+    @WebMethod()
+    public boolean getPlacaVerificar(@WebParam(name="numero") String id) {
+		if(id.equalsIgnoreCase("efg456")) {
+			return true;
+		}else
+			return false;
+		
+    
+    }
 }
